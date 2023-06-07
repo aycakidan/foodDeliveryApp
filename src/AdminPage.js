e5b091a360e61315c95237efbfe1425d58251cf6
feyzanuraltay1 <80525115+feyzanuraltay1@users.noreply.github.com>
2023-06-07 17:59:21 +0300

    Admin page, css made with styled-components

---
+++ src/AdminPage.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import AdminNavbar from "./AdminNavbar";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Footer from "./Footer";
+
+const AdminPage = () => {
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    const handleFoods = async () => {
+      const response = await fetch(`http://localhost:4000/foods`, {
+        method: "GET",
+        json: true,
+      });
+
+      const data = await response.json();
+      setFood(data);
+      console.log(data);
+    };
+    handleFoods();
+  }, []);
+
+  return (
+    <>
+      <AdminContainer>
+        <AdminNavbar />
+
+        <div className="info">
+          <div>
+            <h3>Weekly Sales</h3>
+            <p>$ 15000</p>
+            <h4>Increased by 60%</h4>
+          </div>
+          <div>
+            <h3>Weekly Orders</h3>
+            <p>45,633</p>
+            <h4>Increased by 45%</h4>
+          </div>
+          <div>
+            <h3>Visitors Online</h3>
+            <p>110,577</p>
+            <h4>Increased by 5%</h4>
+          </div>
+        </div>
+        {
+          food.map((item,index)=>{
+            return <div className="food">
+              <div>
+              <img src="" alt="" />
+              <h3>{item.item}</h3>
+              <p>{item.price}$</p>
+              </div>
+              <button>Delete</button>
+            </div>
+          })
+        }
+        <Footer/>
+      </AdminContainer>
+      
+    </>
+  );
+};
+
+const AdminContainer = styled.div`
+  width: 80% !important;
+  
+  .info{
+    display:flex;
+    justify-content: space-between;
+    margin:4rem auto;
+  }
+  @media (max-width:992px){
+    .info{
+      flex-direction: column;
+      width:100%;
+      justify-content: center;
+      align-items: center;
+      row-gap: 1rem;
+      margin:1rem auto;
+    }
+    .info div{
+      width:300px !important;
+      padding:8px;
+    }
+
+  }
+  .info div{
+    width:25%;
+    height:25%;
+    align-items: center;
+  }
+  .info div:nth-child(1){
+    background: #E4DECB;
+    display:flex;
+    
+    flex-direction: column;
+    justify-content: center;
+  }
+  .info div:nth-child(2){
+    background: #C0C2E4;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+  }
+  .info div:nth-child(3){
+    background: #B5E4DD;
+      display:flex;
+
+      flex-direction:column;
+      justify-content: center;
+  }
+
+  .info p{
+    color: green;
+    font-size: large;
+  }
+  .food{
+    background-color:#E59691;
+    margin-top:0.5rem;
+    padding: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .food button{
+    height: 50px;
+    width:120px;
+    font-size: 24px;
+    border:0px;
+    background-color: #E51909;
+    color:white;
+  }
+`;
+
+export default AdminPage;
